491f8740db816e1beb9264f94931dbb83766a9d2
Mitchell Opitz <mitchell@mitchellopitz.net>
2024-06-05 09:59:54 -0500

    refactor - Reorganize project files

--- Editor/Autotiler_SO.cs
+++ Editor/Autotiler_SO.cs
@@ -124,17 +124,3 @@ public class Autotiler_SO : ScriptableObject
 }
 
 #endif
-
-public static class NaturalSortingExtension
-{
-    public static IOrderedEnumerable<T> OrderByNatural<T>(this IEnumerable<T> source, Func<T, string> selector)
-    {
-        int maxLen = source.Select(selector).Select(s => s.Length).Max();
-        return source.OrderBy(s => NaturalSortingExtension.NaturalString(selector(s), maxLen));
-    }
-
-    private static string NaturalString(string str, int maxLen)
-    {
-        return string.Join("\0", Regex.Split(str, "([0-9]+)").Select(s => s.PadLeft(maxLen, '0')));
-    }
-}
