b7b0c02afcc108a84870f4b8cd8ff82d4f370339
Mitchell Opitz <mitchell@mitchellopitz.net>
2024-06-10 09:45:39 -0500

    refactor - General code cleanup after initial project completed

--- Editor/Autotiler_GUI.cs
+++ Editor/Autotiler_GUI.cs
@@ -2,51 +2,64 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(Autotiler_SO))]
-public class Autotiler_GUI : Editor
+public class AutotilerGUI : Editor
 {
-    SerializedProperty tilemaps;
-    SerializedProperty ruleTileTemplate;
-    Autotiler_SO script;
+    private SerializedProperty tilemaps;
+    private SerializedProperty ruleTileTemplate;
+    private Autotiler_SO autotilerScript;
 
-    private void Awake()
+    private void OnEnable()
     {
         tilemaps = serializedObject.FindProperty("tilemaps");
         ruleTileTemplate = serializedObject.FindProperty("ruleTileTemplate");
-        script = (Autotiler_SO)target;
+        autotilerScript = (Autotiler_SO)target;
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
 
-        // Header
-        EditorGUILayout.LabelField("BF Tools - Automatic Rule Tile Generator", EditorStyles.boldLabel);
-        GUI_HorizontalLine();
-
-        // Tilemap
-        EditorGUILayout.LabelField("Provide a tilemap file that follows the image.png template layout.");
-        EditorGUILayout.LabelField("If more than one file is provided, tilemaps will be assigned randomly.");
-        EditorGUILayout.LabelField("Useful for quickly creating visually different tilemaps.");
-        EditorGUILayout.PropertyField(tilemaps);
-        GUI_HorizontalLine();
-
-        // Rule Tile Template
-        EditorGUILayout.LabelField("If you have created a custom rule tile template, you can provide it here.");
-        EditorGUILayout.LabelField("Doing so will allow you to use your own tile layouts rather than the one provided.");
-        EditorGUILayout.LabelField("Warning: this is for more advanced users only.");
-        EditorGUILayout.PropertyField(ruleTileTemplate);
-        GUI_HorizontalLine();
-
-        //Button
-        if (GUILayout.Button("Generate Rule Tile"))
-        {
-            script.CreateRuleTiles();
-        }
+        DrawHeader();
+        DrawTilemapSection();
+        DrawRuleTileTemplateSection();
+        DrawGenerateButton();
 
         serializedObject.ApplyModifiedProperties();
     }
 
-    private static void GUI_HorizontalLine()
+    private void DrawHeader()
+    {
+        EditorGUILayout.LabelField("BF Tools - Automatic Rule Tile Generator", EditorStyles.boldLabel);
+        DrawHorizontalLine();
+    }
+
+    private void DrawTilemapSection()
+    {
+        EditorGUILayout.LabelField("Provide a tilemap file that follows the image.png template layout.");
+        EditorGUILayout.LabelField("If more than one file is provided, tilemaps will be assigned randomly.");
+        EditorGUILayout.LabelField("Useful for quickly creating visually different tilemaps.");
+        EditorGUILayout.PropertyField(tilemaps);
+        DrawHorizontalLine();
+    }
+
+    private void DrawRuleTileTemplateSection()
+    {
+        EditorGUILayout.LabelField("If you have created a custom rule tile template, you can provide it here.");
+        EditorGUILayout.LabelField("Doing so will allow you to use your own tile layouts rather than the one provided.");
+        EditorGUILayout.LabelField("Warning: this is for more advanced users only.");
+        EditorGUILayout.PropertyField(ruleTileTemplate);
+        DrawHorizontalLine();
+    }
+
+    private void DrawGenerateButton()
+    {
+        if (GUILayout.Button("Generate Rule Tile"))
+        {
+            autotilerScript.CreateRuleTiles();
+        }
+    }
+
+    private static void DrawHorizontalLine()
     {
         Rect rect = GUILayoutUtility.GetRect(10, 1, GUILayout.ExpandWidth(true));
         Color lineColor = new Color(0.10196f, 0.10196f, 0.10196f, 1);
--- Runtime/NaturalSortingExtension.cs
+++ Runtime/NaturalSortingExtension.cs
@@ -1,21 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System;
 
-namespace BF_Sort
+namespace BF_Tools
 {
-	public static class NaturalSortingExtension
-	{
-		public static IOrderedEnumerable<T> OrderByNatural<T>(this IEnumerable<T> source, Func<T, string> selector)
-		{
-			int maxLen = source.Select(selector).Select(s => s.Length).Max();
-			return source.OrderBy(s => NaturalSortingExtension.NaturalString(selector(s), maxLen));
-		}
+    public static class NaturalSortingExtension
+    {
+        public static IOrderedEnumerable<T> OrderByNatural<T>(this IEnumerable<T> source, System.Func<T, string> selector)
+        {
+            int maxLen = source.Select(selector).Select(s => s.Length).Max();
+            return source.OrderBy(s => NaturalString(selector(s), maxLen));
+        }
 
-		private static string NaturalString(string str, int maxLen)
-		{
-			return string.Join("\0", Regex.Split(str, "([0-9]+)").Select(s => s.PadLeft(maxLen, '0')));
-		}
-	}
+        private static string NaturalString(string str, int maxLen)
+        {
+            return string.Join("\0", Regex.Split(str, "([0-9]+)").Select(s => s.PadLeft(maxLen, '0')));
+        }
+    }
 }
